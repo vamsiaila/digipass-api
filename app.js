733ffe759b761a1c375b1612bd6d7a5809248b3f
VAMSI AILA <avamsi.web@gmail.com>
2018-08-10 21:12:05 +0530

    db commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const controllers = require('./controllers');
 
-app.use(bodyParser);
-app.use(controllers);
+//database connection
+mongoose.connect("mongodb://digipass:digipass1@ds127589.mlab.com:27589/digipass");
+var db = mongoose.connection;
+db.on('error', ()=>console.log('error connecting database')); //checking for errors
+db.once('open', ()=>console.log('connected to database')); //confirming connected
+
+//middlewares
+app.use('/api',bodyParser);
+app.use('/api',controllers);
 
 
+
+//listening port
 const port = process.env.PORT || 3030;
 app.listen(port,()=>console.log(`app listening to port ${port}`));
