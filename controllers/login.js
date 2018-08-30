4bfc3d121e0b5c5e080c10480ebf1c9c5d005f0e
VAMSI AILA <avamsi.web@gmail.com>
2018-08-30 21:36:17 +0530

    changes

--- controllers/login.js
+++ controllers/login.js
@@ -12,30 +12,7 @@ const schema = {
 }
 
 router.post('/',(req,res)=>{
-    const data = Joi.validate(req.body,schema);
-    if(!data.error){
-        User.get({},(err,res)=>{
-            if(res){
-                const result = res.find(x=> x.username == data.value.usernameOrEmail || x.email == data.value.usernameOrEmail);
-                if(result){
-                    bcrypt.compare(data.value.password,result.password,(err,response)=>{
-                        if(response){
-                            const token = jwt.sign({_id:result._id,username:result.username},"private");
-                            res.send({status:true,token:token,response:'successfully logged in'})
-                        }else{
-                            res.status(400).send({status:false,response:'wrong password'})
-                        }
-                    })
-                }else{
-                    res.status(404).send({status:false,response:'account not found'})
-                }
-            }else{
-                res.status(400).send({status:false,response:'problem from retriving'})
-            }
-        })
-    }else{
-        res.status(400).send({status:false,response:data.error.details[0].message})
-    }
+    
 })
 
 
--- models/users.js
+++ models/users.js
@@ -10,28 +10,4 @@ const userSchema = new mongoose.Schema({
     admin:Boolean
 })
 
-const User = mongoose.model('User',userSchema);
-
-exports.create = async function (data,callback) {
-   const user = new User(data);
-   const result = await user.save();
-   callback(null,result);
-}
-
-exports.get = async function(req,callback){
-    User.find(req.match,req.fields,(err,res)=>{
-        callback(err,res);
-    })
-}
-
-exports.update = async function(req,callback){
-    User.update(req,match,req.fields,(err,res)=>{
-        callback(err,res);
-    })
-}
-
-exports.delete = async function(req,callback){
-    User.remove(req,(err,res)=>{
-        callback(err,res);
-    })
-}
+module.exports = mongoose.model('User',userSchema);
